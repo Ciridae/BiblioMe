cb0486aa8eca3bff3a29c5196245036d75cfb2a3
Abel Domínguez Morejón <temausdomo@gmail.com>
2023-05-03 20:36:53 +0200

    Comentar en el modelo de Autor el atributo de libros, no funciona

--- src/main/java/com/daw/bibliome/dao/modelo/Autor.java
+++ src/main/java/com/daw/bibliome/dao/modelo/Autor.java
@@ -1,13 +1,10 @@
 package com.daw.bibliome.dao.modelo;
 
-import java.util.List;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
@@ -43,9 +40,9 @@ public class Autor {
 	@Column(name = "biografia", nullable = true, length = 2000)
 	private String biografia;
 
-	@OneToMany(targetEntity = Libro.class, mappedBy = "autor")
-	// @JoinColumn(name = "libros", referencedColumnName = "id")
-	private List<Libro> libros;
+	/*
+	 * @OneToMany(mappedBy="autor") private List<Libro> libros;
+	 */
 
 	public Autor() {
 		super();
@@ -64,6 +61,15 @@ public class Autor {
 		this.autorDesde = autorDesde;
 		this.biografia = biografia;
 	}
+
+	/*
+	 * public Autor(Integer id, String nombre, String apellidos, String img, String
+	 * nacido, String web, String twitter, String autorDesde, String biografia,
+	 * List<Libro> libros) { super(); this.id = id; this.nombre = nombre;
+	 * this.apellidos = apellidos; this.img = img; this.nacido = nacido; this.web =
+	 * web; this.twitter = twitter; this.autorDesde = autorDesde; this.biografia =
+	 * biografia; this.libros = libros; }
+	 */
 
 	public Integer getId() {
 		return id;
@@ -137,12 +143,10 @@ public class Autor {
 		this.biografia = biografia;
 	}
 
-	public List<Libro> getLibros() {
-		return libros;
-	}
-
-	public void setLibros(List<Libro> libros) {
-		this.libros = libros;
-	}
+	/*
+	 * public List<Libro> getLibros() { return libros; }
+	 * 
+	 * public void setLibros(List<Libro> libros) { this.libros = libros; }
+	 */
 
 }
